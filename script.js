5ee25875bc9c950d6b7a0085c355015701b2fa4c
forestmeichtry <fmeichtry@gmail.com>
2023-01-30 19:16:06 -0800

    -Add display books with 3d transform on hover

--- script.js
+++ script.js
@@ -18,8 +18,27 @@ addBookToLibrary('Lord of the Flies', 'William Golding');
 
 // Adds card to screen for each book in array
 for (book of bookArray) {
-    let newItem = document.createElement('div');
-    newItem.innerHTML = `Title: ${book.title}, Author: ${book.author}`
-    newItem.classList.add('bookCard');
-    library.appendChild(newItem);
+    let card = document.createElement('div');
+    card.classList.add('bookCard');
+    library.appendChild(card);
+
+    let bookCover = document.createElement('div');
+    bookCover.classList.add('bookCover');
+    card.appendChild(bookCover);
+
+    let bookTop = document.createElement('div');
+    bookTop.classList.add('bookTop');
+    bookCover.appendChild(bookTop);
+
+    let titleField = document.createElement('span');
+    titleField.innerHTML = book.title;
+    bookTop.appendChild(titleField);
+
+    let authorField = document.createElement('span');
+    authorField.innerHTML = book.author;
+    bookTop.appendChild(authorField);
+    
+    let bookSide = document.createElement('div');
+    bookSide.classList.add('bookSide');
+    bookCover.appendChild(bookSide);
 }
